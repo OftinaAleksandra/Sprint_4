f7057b7c054a04bab2f5f6f2e429c264f2717877
Aleksandra Oftina <aoftina@it-one.ru>
2023-03-20 12:52:29 +0300

    project  Sprint_4

---
+++ src/main/java/PageObject/PersonScooter.java
@@ -0,0 +1,35 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PersonScooter {
+    private final By fieldName = By.xpath(".//input[@class='Input_Input__1iN_Z Input_Responsible__1jDKN' and @placeholder='* Имя']");
+    private final By fieldSurname = By.xpath(".//input[@class='Input_Input__1iN_Z Input_Responsible__1jDKN' and @placeholder='* Фамилия']");
+    private final By fieldAddress = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
+    private final By buttonSubway = By.xpath(".//input[@placeholder='* Станция метро']");
+    private final String selectSubwayStation = ".//div[@class='select-search__select']//*[text()='%s']";
+    private final By fieldPhoneNumber = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
+    private final By buttonNextStep = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    private final WebDriver driver;
+
+
+    public PersonScooter(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void fillForm(String name, String surname, String address, String subwayStation, String phoneNumber) {
+        driver.findElement(fieldName).sendKeys(name);
+        driver.findElement(fieldSurname).sendKeys(surname);
+        driver.findElement(fieldAddress).sendKeys(address);
+        driver.findElement(buttonSubway).sendKeys(subwayStation);
+        String currentSubwayStation = String.format(selectSubwayStation, subwayStation);
+        driver.findElement(By.xpath(currentSubwayStation)).click();
+        driver.findElement(fieldPhoneNumber).sendKeys(phoneNumber);
+    }
+
+    public void clickButtonNextStep() {
+        driver.findElement(buttonNextStep).click();
+    }
+
+}
---
+++ src/test/java/OrderScooterTest.java
@@ -0,0 +1,94 @@
+import PageObject.ConfirmationOrder;
+import PageObject.MainPage;
+import PageObject.PersonScooter;
+import PageObject.RentScooter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@RunWith(Parameterized.class)
+public class OrderScooterTest {
+    private final WebDriver driver = new ChromeDriver();
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String numberPhone;
+    private final String subwayStation;
+    private final String rentalPeriod;
+    private final String colorScooter;
+    private final String comment;
+
+    public OrderScooterTest(String name, String surname, String address, String numberPhone, String subwayStation, String rentalPeriod, String colorScooter, String comment) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.numberPhone = numberPhone;
+        this.subwayStation = subwayStation;
+        this.rentalPeriod = rentalPeriod;
+        this.colorScooter = colorScooter;
+        this.comment = comment;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getPersonalData() {
+        return new Object[][]{
+                {"Иванов", "Иван","Москва, ул. Академика Королёва, 12","89889999999","Сокольники","сутки", "серая безысходность", "Позвонить заранее"},
+                {"Сидоров", "Семен", "Санкт-Петербург", "89185361245", "Преображенская площадь", "двое суток", "чёрный жемчуг", "no comment"},
+        };
+    }
+
+    @Before
+    public void setUp() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open();
+    }
+
+    @Test
+    public void checkOrderScooterOnHeaderButton() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickOnUpperOrder();
+        PersonScooter personScooter = new PersonScooter(driver);
+        personScooter.fillForm(name, surname, address, subwayStation, numberPhone);
+        personScooter.clickButtonNextStep();
+        RentScooter rentScooter = new RentScooter(driver);
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("Order_Header__BZXOb")));
+        rentScooter.fillRentScooterFormWithGreyScooter(rentalPeriod, colorScooter, comment);
+        rentScooter.clickOrderButton();
+        ConfirmationOrder confirmationOrder = new ConfirmationOrder(driver);
+        confirmationOrder.clickAgreeConfirmationOrder();
+        confirmationOrder.successOrder();
+
+    }
+
+    @Test
+    public void checkOrderScooterOnLowerButton() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickOnLowerOrder();
+        PersonScooter personScooter = new PersonScooter(driver);
+        personScooter.fillForm(name, surname, address, subwayStation, numberPhone);
+        personScooter.clickButtonNextStep();
+        RentScooter rentScooter = new RentScooter(driver);
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("Order_Header__BZXOb")));
+        rentScooter.fillRentScooterFormWithGreyScooter(rentalPeriod, colorScooter, comment);
+        rentScooter.clickOrderButton();
+        ConfirmationOrder confirmationOrder = new ConfirmationOrder(driver);
+        confirmationOrder.clickAgreeConfirmationOrder();
+        confirmationOrder.successOrder();
+
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+}
